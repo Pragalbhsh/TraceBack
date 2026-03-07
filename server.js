4244c111bce16e281dc4ebba31eb87d3e14517bd
Pragalbh <pragalbhsharma11@gmail.com>
2026-03-08 04:18:45 +0530

    connected backend to mongoDB

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://tracebackuser:traceback123@cluster0.5vaf3gr.mongodb.net/?appName=Cluster0")
+    .then(()=>{
+        console.log("connected to mongodb");
+    })
+    .catch((err)=>{
+        console.log("error connecting to mongodb" , err);
+    });
+
+    const itemsSchema = new mongoose.Schema({
+        name:String,
+        description :String,
+        location:String,
+        contact :String,
+        date:Date
+    });
+    const item = mongoose.model("items" , itemsSchema);
+
+
+
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -10,25 +30,49 @@ app.get('/', (req,res) => {
 let items  = []; // empty array
 
 // add new items
-app.post("/items", (req,res) => { 
-   const newitem = req.body; // read data
-   items.push(newitem); // add data to array
-   res.send("item added");
+app.post("/items", async(req,res) => { 
+    try{
+   const newItem = new item(req.body);
+   const savedItem = await newItem.save(); // save data to mongodb
+   res.json(savedItem);
+    }catch(err){
+        console.log(err);
+        res.status(500).send("error saving item");
+    }
 });
 
 // see all items
-app.get("/items", (req,res) => {
-res.json(items);
+app.get("/items", async (req,res) => {
+    try{
+        const items = await item.find();
+        res.json(items);
+    }catch(err){
+        console.log(err);
+        res.status(500).send("error fetching items");
+    }
+});
+
+
+// see a single item
+
+app.get("/items/:id", async (req,res) => {
+    try{
+        const founditem = await item.findById(req.params.id);
+        res.json(founditem);
+    }catch(err){
+        console.log(err);
+        res.status(500).send("error fetching item");
+    }
 });
 
 //update items
 app.put('/items/:id' , (req,res) => {
 const id = parseInt(req.params.id) // getting item id from the url
-const index = items.findIndex(item => item.id === id); // check index of the item in our array
+const index = items.findIndex(items => items.id === id); // check index of the item in our array
 if(index === -1){
     return res.status(404).send("item not found");
 }
-item[index] = {... item[index] , ...req.body} // update the item as requested
+items[index] = { ...items[index] , ...req.body} // update the item as requested
 res.send("item updated");
 });
 
@@ -36,9 +80,9 @@ res.send("item updated");
 // delete items
 app.delete('/items/:id', (req,res) =>{
 const id = parseInt(req.params.id);
-const lengthBefore = items.length();
-items = items.filter(item=>item.id !== id); // filter fucntion doesnt harm the items that dont have the given id and deletes the item with the given id
-if(lengthBefore === item.length()){
+const lengthBefore = items.length;
+items = items.filter(i=>i.id !== id); // filter fucntion doesnt harm the items that dont have the given id and deletes the item with the given id
+if(lengthBefore === items.length){
     return res.status(400).send("item not found");
 }
 res.send("item deleted");
